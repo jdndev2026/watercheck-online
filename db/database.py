cde7717c05c11d1e52fd64d8206ab0050577eccb
jdndev2026 <jdndev2026@gmail.com>
2026-05-24 18:46:12 -0500

    correccion variable de entorno database

--- db/database.py
+++ db/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./db/database.db")
 
 engine = create_engine( SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread' : False} )
 
